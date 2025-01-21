f3f8b52799fc9f15de82e0c5f014b523588b1f84
Francesco Prosperi mangili <fprosper@e4r8p1.42roma.it>
2025-01-21 17:48:34 +0100

    modulo00

--- cpp_module_00/Warlock.cpp
+++ cpp_module_00/Warlock.cpp
@@ -1,41 +1,19 @@
 #include "Warlock.hpp"
 
-std::string     const &Warlock::getName() const
-{
-    std::cout << _name << std::endl;
-}
-
-std::string     const &Warlock::getTitle() const
-{
-    std::cout << _title << std::endl;
-}
-
-void      Warlock::setTitle(std::string const &str)
-{
-    _title = str;
-}
-
-void    Warlock::introduce() const
-{
-    std::cout << getName() << ": I am" << getName() << "," << getTitle() << "!" << std::endl;
-}
-
-
-
-// Coplien form
-
-Warlock::Warlock()                                                                      
+Warlock::Warlock()
 {
 }
 
-Warlock::Warlock(std::string name, std::string title): _name(name); _title(title)
+Warlock::Warlock(std::string name, std::string title): _name(name), _title(title)
 {
-    std::cout << getName() << ": This looks like another boring day." << std::endl;
+    std::cout << _name << ": This looks like another boring day." << std::endl; 
 }
 
-Warlock::~Warlock()
+Warlock &Warlock::operator=(Warlock const &rhs)
 {
-    std::cout << getName() << ": My job here is done!" << std::endl;
+    this->_name = rhs._name;
+    this->_title = rhs._title;
+    return (*this);
 }
 
 Warlock::Warlock(Warlock const &obj)
@@ -43,4 +21,27 @@ Warlock::Warlock(Warlock const &obj)
     *this = obj;
 }
 
-(Warlock const &rho)
+Warlock::~Warlock()
+{
+    std::cout << _name << ": My job here is done!" << std::endl;
+}
+
+std::string const & Warlock::getName() const
+{
+    return (_name);
+}
+
+std::string const & Warlock::getTitle() const
+{
+    return(_title);
+}
+
+void    Warlock::setTitle(std::string const &str)
+{
+    _title = str;
+}
+
+void    Warlock::introduce() const
+{
+    std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+}
--- cpp_module_00/Warlock.hpp
+++ cpp_module_00/Warlock.hpp
@@ -5,15 +5,19 @@ class Warlock
     private:
         std::string _name;
         std::string _title;
-        Warlock(Warlock const &obj);                     // Copy constructor
-        Warlock &operator=(Warlock const &rho);          // Copy assigment oeprator overload 
-        Warlock();                                       // Default constructor
+        Warlock();
+        Warlock(Warlock const &obj);
+        Warlock &operator=(Warlock const &rhs);
 
     public:
+        Warlock(std::string name, std::string title);
+        ~Warlock();
+        
         std::string const &getName() const;
         std::string const &getTitle() const;
-        Warlock (std::string name, std::string title);   // Custom costructor
-        ~Warlock();                                      // Destructor
         void    setTitle(std::string const &str);
-        void    introduce () const;
-};
+        void    introduce() const;
+        
+
+
+};  
